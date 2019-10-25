cb0dceefae239fbd5a6aca0b0081097b33c729fb
ybxiang05 <brooke.xiang@gmail.com>
2019-10-25 16:54:47 -0700

    fix newsspan display; filter 12 items

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+//get value from selector
+//take and make lowercase
+//console log data
+//make sure all articles have images
+//we only want 12 articles
+// go through each object and get info for each article
+//append to dom
+
 $(function() {
     $('.dropdown-button').on('change', function(){
         const selectionValue = this.value.toLowerCase();
@@ -7,29 +15,24 @@ $(function() {
             url: `https://api.nytimes.com/svc/topstories/v2/${selectionValue}.json?api-key=ALcL70xVAiEjjiKsqBxMnAX2VdWNKIIK&limit=12`,
             dataType: 'json',
         }) 
-        // use empty array newsItems = []; if <=12, push to array, and append to .news-span
         .done(function(data) {
-            // console.log(data);
             $('.news-span').empty();
-            $.each(data.results, function (key, value) {
-                // const newsItems = [];
-                if (value.multimedia.length !==0 /**newsItems.length < 13**/) {
-                    const str=`<div class='bg-img' style='background-image: url(${value.multimedia[4].url})'>
-                        <h3 class='article-title article-text'>${value.title}</h3>
-                        <p class='article-abstract article-text'>${value.abstract}</p>
-                    </div>`
-                    // newsItems.push(str);
-                    // console.log(newsItems);
-                    $('.news-span').append(str)
-                    
-                    // $('.news-span').append(`<p class="bg-img" style="background-image: url(${value.multimedia[4].url})">`)
-                    // $(`.bg-img`).append(`<p>${this.title}</p>`)
-                }
-
-                    // for each create new <div> for just background image; append value.title to background to create that second box??
-
-
-            }) 
+            const filteredImages = data.results.filter(function(event){
+                if (event.multimedia[4] !==undefined) {
+                    return true
+                }else{
+                    return false;}});
+            const slicedArticles = filteredImages.slice(0, 12);
+            console.log(slicedArticles);
+            $.each(slicedArticles, function (key, value) {
+                const str = `<a class='article-url' href='${value.url}'>
+                                <div class='bg-img' style='background-image: url(${value.multimedia[4].url})'>
+                                    <h3 class='article-title article-text'>${value.title}</h3>
+                                    <p class='article-abstract article-text'>${value.abstract}</p>
+                                </div>
+                            </a>`
+                $('.news-span').append(str)
+            }); 
             //  $( 'header img' ).remove()
         })
     });
