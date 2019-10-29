e1a234af9ba1db6f07a9f476566691b95c6527ce
ybxiang05 <brooke.xiang@gmail.com>
2019-10-29 14:43:54 -0700

    add loader gif basic css animation

--- js/script.js
+++ js/script.js
@@ -1,51 +1,53 @@
+//
+//put in loader.gif
 //get value from selector
 //take and make lowercase
-//console log data
+//***** if statement for if selections is chosen, if so, do not run ajax
 //make sure all articles have images
 //we only want 12 articles
 // go through each object and get info for each article
 //append to dom
+//remove loader.gif
+//***** add .fail(function)
 
 $(function() {
   $(".dropdown-button").on("change", function() {
-    $(".nyt-logo").addClass("change");
-    $(".category-selection").addClass("change");
-    const selectionValue = this.value.toLowerCase();
-    //on change, make logo small??
-    // if ($(window).width() > 600) {
-    //   $(".nyt-logo").css("width", "75px");
-    // } else {
-    //   $(".nyt-logo").css("width", "150px");
-    // }
+    $(".header").append(
+      '<img class="loader-gif" src="assets/images/ajax-loader.gif" />'
+    );
 
-    // $('header').append('<img src="loader.gif" />')
+    const selectionValue = this.value.toLowerCase();
+
+    // TODO check if selected val is "sections" and dont run ajax if it is
+    // return;
+
     $.ajax({
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${selectionValue}.json?api-key=ALcL70xVAiEjjiKsqBxMnAX2VdWNKIIK&limit=12`,
       dataType: "json"
-    }).done(function(data) {
-      $(".news-span").empty();
-      const filteredImages = data.results.filter(function(event) {
-        if (event.multimedia[4] !== undefined) {
-          return true;
-        } //else {
-        //   return false;
-        // }
-      });
-      const slicedArticles = filteredImages.slice(0, 12);
-      console.log(slicedArticles);
-      $.each(slicedArticles, function(key, value) {
-        const str = `<a class='bg-img' style='background-image: url(${value.multimedia[4].url})' href ='${value.url}'>
+    })
+      .done(function(data) {
+        $(".nyt-logo").addClass("change");
+        $(".category-selection").addClass("change");
+        $(".news-span").empty();
+        const filteredImages = data.results.filter(function(event) {
+          if (event.multimedia[4] !== undefined) {
+            return true;
+          }
+        });
+        const slicedArticles = filteredImages.slice(0, 12);
+        console.log(slicedArticles);
+        $("header .loader-gif").remove();
+        $.each(slicedArticles, function(key, value) {
+          const str = `<a class='bg-img' style='background-image: url(${value.multimedia[4].url})' href ='${value.url}'>
             <h3 class='article-title article-text'>${value.title}</h3>
             <p class='article-abstract article-text'>${value.abstract}</p>
         </a>`;
-        $(".news-span").append(str);
+          $(".news-span").append(str);
+        });
+      })
+      .fail(function() {
+        console.log("something went wrong");
       });
-    });
-    //  $( 'header img' ).remove()
   });
 });
-$(".bg-img").on("hover", function visibility() {
-  $(".article-abstract").classList.toggle("show");
-});
-// });
