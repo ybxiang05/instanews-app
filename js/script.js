d5fc597e7a6a57acfc5d184d9e9a50ee015f7484
ybxiang05 <brooke.xiang@gmail.com>
2019-10-29 16:06:17 -0700

    fix css add if/else selections

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 //we only want 12 articles
 // go through each object and get info for each article
 //append to dom
-//remove loader.gif
+//***** - deferred .always remove loader.gif
 //***** add .fail(function)
 
 $(function() {
@@ -20,34 +20,40 @@ $(function() {
 
     // TODO check if selected val is "sections" and dont run ajax if it is
     // return;
-
-    $.ajax({
-      method: "GET",
-      url: `https://api.nytimes.com/svc/topstories/v2/${selectionValue}.json?api-key=ALcL70xVAiEjjiKsqBxMnAX2VdWNKIIK&limit=12`,
-      dataType: "json"
-    })
-      .done(function(data) {
-        $(".nyt-logo").addClass("change");
-        $(".category-selection").addClass("change");
-        $(".news-span").empty();
-        const filteredImages = data.results.filter(function(event) {
-          if (event.multimedia[4] !== undefined) {
-            return true;
-          }
-        });
-        const slicedArticles = filteredImages.slice(0, 12);
-        console.log(slicedArticles);
-        $("header .loader-gif").remove();
-        $.each(slicedArticles, function(key, value) {
-          const str = `<a class='bg-img' style='background-image: url(${value.multimedia[4].url})' href ='${value.url}'>
-            <h3 class='article-title article-text'>${value.title}</h3>
-            <p class='article-abstract article-text'>${value.abstract}</p>
-        </a>`;
-          $(".news-span").append(str);
-        });
+    if (selectionValue !== "sections") {
+      $.ajax({
+        method: "GET",
+        url: `https://api.nytimes.com/svc/topstories/v2/${selectionValue}.json?api-key=ALcL70xVAiEjjiKsqBxMnAX2VdWNKIIK&limit=12`,
+        dataType: "json"
       })
-      .fail(function() {
-        console.log("something went wrong");
-      });
+        .done(function(data) {
+          $(".nyt-logo").addClass("change");
+          $(".category-selection").addClass("change");
+          $(".news-span").empty();
+          const filteredImages = data.results.filter(function(event) {
+            if (event.multimedia[4] !== undefined) {
+              return true;
+            }
+          });
+          const slicedArticles = filteredImages.slice(0, 12);
+          console.log(slicedArticles);
+          $("header .loader-gif").remove();
+          $.each(slicedArticles, function(key, value) {
+            const str = `<a class='bg-img' style='background-image: url(${value.multimedia[4].url})' href ='${value.url}'>
+          <div class='article-text'>
+            <h3 class='article-title'>${value.title}</h3>
+            <p class='article-abstract'>${value.abstract}</p>
+          </div>
+        </a>`;
+            $(".news-span").append(str);
+          });
+        })
+        .fail(function() {
+          console.log("something went wrong");
+        });
+    } else {
+      $(".news-span").empty();
+      console.log("selections");
+    }
   });
 });
